347323b2a6b22f6b71878a7932686195d0a05f1a
Kamran Jornacion <Kamran.Jornacion@gmail.com>
2025-10-07 21:20:48 -0400

    done!

--- Assign 2/fraction_22QFC4.cpp
+++ Assign 2/fraction_22QFC4.cpp
@@ -1,13 +1,17 @@
 #include "fraction_22QFC4.h"
 #include <cstdlib>
-
 using namespace std;
 
+//helper to prevent overflow during multiplication
 static inline long long cross(long long a, long long b) { return a * b; }
 
+//Fraction Methods and constructors ----------------
+
+//Uses Eulid's algo to find gcd
 int Fraction::gcd(int a, int b) {
-    a = std::abs(a);
-    b = std::abs(b);
+
+    a = abs(a);
+    b = abs(b);
     while (b != 0) {
         int r = a % b;
         a = b;
@@ -16,41 +20,50 @@ int Fraction::gcd(int a, int b) {
     return a;
 }
 
+
 void Fraction::normalize() {
-    if (denom == 0) throw FractionException("denominator cannot be zero");
+    //verify valid denom
+    if (denom == 0) throw FractionException("denominator can't be zero");
+    //format to ensure numerator carries sign
     if (num == 0) { denom = 1; return; }
     if (denom < 0) { denom = -denom; num = -num; }
+    //reduce fraction
     int g = gcd(num, denom);
     if (g) { num /= g; denom /= g; }
 }
 
+//Construct as 0/1
 Fraction::Fraction() : num(0), denom(1) {}
 
+//Construct as n/1
 Fraction::Fraction(int n) : num(n), denom(1) {}
 
+//Construct as n/d then normalize format
 Fraction::Fraction(int n, int d) : num(n), denom(d) { normalize(); }
 
+//Access numerator or denominator accordingly
 int Fraction::numerator() const { return num; }
-
 int Fraction::denominator() const { return denom; }
 
+//Overwrite urnary minus
 Fraction Fraction::operator-() const {
     Fraction r(*this);
     r.num = -r.num;
     return r;
 }
 
+ //overwrite pre-increment
 Fraction& Fraction::operator++() {
     num += denom;
     return *this;
 }
-
+//overwrite post-increment
 Fraction Fraction::operator++(int) {
     Fraction tmp(*this);
     num += denom;
     return tmp;
 }
-
+//adds rhs to this fraction (rhs is Fraction)
 Fraction& Fraction::operator+=(const Fraction& rhs) {
     long long n = cross(num, rhs.denom) + cross(rhs.num, denom);
     long long d = cross(denom, rhs.denom);
@@ -59,19 +72,19 @@ Fraction& Fraction::operator+=(const Fraction& rhs) {
     normalize();
     return *this;
 }
-
+ //adds rhs to this fraction (rhs is integer)
 Fraction& Fraction::operator+=(int rhs) {
     long long n = num + cross(static_cast<long long>(rhs), denom);
     num = static_cast<int>(n);
     normalize();
     return *this;
 }
-
+//output as "num/denom" or "num" when denom is 1
 ostream& operator<<(ostream& os, const Fraction& f) {
     os << f.num << "/" << f.denom;
     return os;
 }
-
+//input as "num1/num2" or "num1" (and check num2 nonzero)
 istream& operator>>(istream& is, Fraction& f) {
     int n = 0, d = 1;
     if (!(is >> n)) { is.setstate(ios::failbit); return is; }
@@ -85,7 +98,7 @@ istream& operator>>(istream& is, Fraction& f) {
     f.normalize();
     return is;
 }
-
+//Overwrites binary +,-,*,/ and returns reduced form---------------
 Fraction operator+(const Fraction& a, const Fraction& b) {
     Fraction r;
     long long n = cross(a.num, b.denom) + cross(b.num, a.denom);
@@ -95,7 +108,6 @@ Fraction operator+(const Fraction& a, const Fraction& b) {
     r.normalize();
     return r;
 }
-
 Fraction operator-(const Fraction& a, const Fraction& b) {
     Fraction r;
     long long n = cross(a.num, b.denom) - cross(b.num, a.denom);
@@ -105,7 +117,6 @@ Fraction operator-(const Fraction& a, const Fraction& b) {
     r.normalize();
     return r;
 }
-
 Fraction operator*(const Fraction& a, const Fraction& b) {
     Fraction r;
     long long n = cross(a.num, b.num);
@@ -115,7 +126,6 @@ Fraction operator*(const Fraction& a, const Fraction& b) {
     r.normalize();
     return r;
 }
-
 Fraction operator/(const Fraction& a, const Fraction& b) {
     if (b.num == 0) throw FractionException("division by zero fraction");
     Fraction r;
@@ -126,27 +136,29 @@ Fraction operator/(const Fraction& a, const Fraction& b) {
     r.normalize();
     return r;
 }
+//---------------------------------------------------------------
 
+//Check equality
 bool operator==(const Fraction& a, const Fraction& b) {
     return cross(a.num, b.denom) == cross(b.num, a.denom);
 }
-
+//Check inequality
 bool operator!=(const Fraction& a, const Fraction& b) {
     return !(a == b);
 }
-
+//Checks less-than using cross-multiplication
 bool operator<(const Fraction& a, const Fraction& b) {
     return cross(a.num, b.denom) < cross(b.num, a.denom);
 }
-
+//checks less or equal
 bool operator<=(const Fraction& a, const Fraction& b) {
     return !(b < a);
 }
-
+//checks greater than
 bool operator>(const Fraction& a, const Fraction& b) {
     return b < a;
 }
-
+//checks greater than or equal
 bool operator>=(const Fraction& a, const Fraction& b) {
     return !(a < b);
 }
--- Assign 2/fraction_22QFC4.h
+++ Assign 2/fraction_22QFC4.h
@@ -1,51 +1,78 @@
-#pragma once
+#ifndef FRACTION_22QFC4_H
+#define FRACTION_22QFC4_H
+
 #include <stdexcept>
 #include <iostream>
 #include <string>
 using namespace std;
 
+//class that stores fraction
 class Fraction{
 private:
+    //numerator of frac
     int num;
+    //denominator of frace
     int denom;
-    void normalize();
+
+    //computes the greatest common divisor 
     static int gcd(int a, int b);
+    //uses gcd to reduce fraction, and keep sign with numerator
+    void normalize();
 
 public:
-
+    //Fraction constuctor from integer
     Fraction(int num);
-    Fraction(int num1, int num2);
+    //Fraction constructor from numerator and denonimator
+    Fraction(int numerator, int denominator);
+
+    //Fraction constructor defaults to 0/1
     Fraction();
 
+    //returns numerator and denominator accordingly
     int numerator() const;
     int denominator() const;
 
+    //overwrite urnary minus
     Fraction operator-() const;
-
+    //overwrite pre-increment
     Fraction& operator++();
+    //overwrite post-increment
     Fraction operator++(int);
-
+    //adds rhs to this fraction (rhs is Fraction)
     Fraction& operator+=(const Fraction& rhs);
+    //adds rhs to this fraction (rhs is integer)
     Fraction& operator+=(int rhs);
-
+    //output as "num/denom" or "num" when denom is 1
     friend ostream& operator<<(ostream& os, const Fraction& f);
+    //input as "num1/num2" or "num1" (and check num2 nonzero)
     friend istream& operator>>(istream& is, Fraction& f);
 
+    //Overwrites binary +,-,*,/ and returns reduced form
     friend Fraction operator+(const Fraction& a, const Fraction& b);
     friend Fraction operator-(const Fraction& a, const Fraction& b);
     friend Fraction operator*(const Fraction& a, const Fraction& b);
     friend Fraction operator/(const Fraction& a, const Fraction& b);
 
+    //Checks equality after normalization
     friend bool operator==(const Fraction& a, const Fraction& b);
+    //Checks inequality after normalization
     friend bool operator!=(const Fraction& a, const Fraction& b);
+    //Checks less-than using corss-multiplication
     friend bool operator<(const Fraction& a, const Fraction& b);
+    //checks less or equal
     friend bool operator<=(const Fraction& a, const Fraction& b);
+    //checks greater than
     friend bool operator>(const Fraction& a, const Fraction& b);
+    //checks greater than or equal
     friend bool operator>=(const Fraction& a, const Fraction& b);
 
 };
 
+//Custom Exception class that is used to indicate when invalid fraction operation/state occurs
 class FractionException: public runtime_error{
 public:
+    //use runtime_error constructors
     using runtime_error::runtime_error;
 };
+
+#endif // FRACTION_22QFC4_H
